7a8c89bc9f30ce9cff1b27fe8940fb6c235e44ca
gaygauta <gautam.gayatri@nokia.com>
2016-04-29 12:47:43 +0530

    Incorporating the review comments,and comparing as per the standard of other class. Also checked the styling

--- apis/openstack-heat/src/main/java/org/jclouds/openstack/heat/v1/domain/Attributes.java
+++ apis/openstack-heat/src/main/java/org/jclouds/openstack/heat/v1/domain/Attributes.java
@@ -3,182 +3,187 @@
  */
 package org.jclouds.openstack.heat.v1.domain;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.beans.ConstructorProperties;
+import java.util.Set;
 
 import javax.inject.Named;
 
 import org.jclouds.openstack.v2_0.domain.Link;
-import org.jclouds.openstack.v2_0.services.Image;
+import org.jclouds.openstack.v2_0.domain.Resource;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Objects.ToStringHelper;
 
 /**
  * @author deviyada
- *
+ * 
  */
-public class Attributes {
+public class Attributes extends Resource {
 
-	@Named("id")
-	private String id;
-	@Named("progress")
 	private String progress;
-	@Named("status")
 	private String status;
 	@Named("config_drive")
-	private String config_drive;
+	private String configDrive;
 	@Named("tenant_id")
-	private String tenant_id;
-	@Named("hostId")
+	private String tenantId;
+	@Named("host_id")
 	private String hostId;
-	@Named("created")
 	private String created;
-	@Named("name")
-	private String name;
 	@Named("user_id")
-	private String user_id;
-	@Named("links")
-	private Link[] links;
+	private String userId;
+
+	@ConstructorProperties({ "id", "name","links","progress", "status", "config_drive",
+			"tenant_id", "host_id", "created", "user_id"})
+	protected Attributes(String id,String name, Set<Link> links, String progress, String status,
+			String configDrive, String tenantId, String hostId,String created, String userId) {
+		super(id, name, links);
+		this.progress = progress;
+		this.status = status;
+		this.configDrive = configDrive;
+		this.tenantId = tenantId;
+		this.hostId = hostId;
+		this.created = created;
+		this.userId = userId;
+	}
 
 	public String getProgress() {
 		return progress;
-	}
-
-	public void setProgress(String progress) {
-		this.progress = progress;
 	}
 
 	public String getStatus() {
 		return status;
 	}
 
-	public void setStatus(String status) {
-		this.status = status;
+	public String getConfigDrive() {
+		return configDrive;
 	}
 
-	public String getConfig_drive() {
-		return config_drive;
-	}
-
-	public void setConfig_drive(String config_drive) {
-		this.config_drive = config_drive;
-	}
-
-	public String getTenant_id() {
-		return tenant_id;
-	}
-
-	public void setTenant_id(String tenant_id) {
-		this.tenant_id = tenant_id;
+	public String getTenantId() {
+		return tenantId;
 	}
 
 	public String getHostId() {
 		return hostId;
 	}
 
-	public void setHostId(String hostId) {
-		this.hostId = hostId;
-	}
-
-	public Link[] getLinks() {
-		return links;
-	}
-
-	public void setLinks(Link[] links) {
-		this.links = links;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
 	public String getCreated() {
 		return created;
 	}
 
-	public void setCreated(String created) {
-		this.created = created;
+	public String getUserId() {
+		return userId;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-
-	public Attributes build() {
-		return new Attributes(id, progress, status, config_drive, tenant_id, hostId, created, user_id, links);
-	}
-
-	@ConstructorProperties({ "id", "progress", "status", "config_drive", "tenant_id", "hostId", "created", "user_id",
-			"links"})
-	protected Attributes(String id, String progress, String status, String config_drive, String tenant_id,
-			String hostId, String created, String user_id, Link[] links) {
-		this.id = checkNotNull(id, "id");
-		this.progress = progress;
-		this.status = status;
-		this.config_drive = config_drive;
-		this.tenant_id = tenant_id;
-		this.hostId = hostId;
-		this.created = created;
-		this.user_id = user_id;
-		this.links = links;
-	}
-
+	
 	@Override
 	public int hashCode() {
-		return Objects.hashCode(id, tenant_id,hostId,status,progress,config_drive,created,user_id,links);
+		return Objects.hashCode(super.hashCode(), progress, status, configDrive, tenantId,
+				hostId, created,userId);
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
 		Attributes that = Attributes.class.cast(obj);
-		return 	   Objects.equal(this.id, that.id) 
-				&& Objects.equal(this.tenant_id, that.tenant_id)
-				&& Objects.equal(this.hostId, that.hostId)
+		return super.equals(that) 
+				&&  Objects.equal(this.progress, that.progress)
 				&& Objects.equal(this.status, that.status)
-				&& Objects.equal(this.progress, that.progress)
-				&& Objects.equal(this.config_drive, that.config_drive)
+				&& Objects.equal(this.configDrive, that.configDrive)
+				&& Objects.equal(this.tenantId, that.tenantId)
+				&& Objects.equal(this.hostId, that.hostId)
 				&& Objects.equal(this.created, that.created)
-				&& Objects.equal(this.user_id, that.user_id)
-				&& Objects.equal(this.links, that.links);
+				&& Objects.equal(this.userId, that.userId);
 	}
 
 	protected ToStringHelper string() {
-		return Objects.toStringHelper(this).add("id", id)
-										  .add("tenant_id", tenant_id)
-										  .add("hostId", hostId)
-										  .add("status", status)
-										  .add("progress", progress)
-										  .add("config_drive", config_drive)
-										  .add("created", created)
-										  .add("user_id", user_id)
-										  .add("links", links);
+		return	super.string()
+				.add("progress", progress).add("status", status)
+				.add("config_drive", configDrive).add("tenant_id", tenantId)
+				.add("host_id", hostId).add("created", created)
+				.add("user_id", userId);
 	}
 
-	@Override
-	public String toString() {
-		return string().toString();
+	 @Override
+	   public String toString() {
+	      return string().toString();
+	   }
+
+	   public static Builder<?> builder() {
+	      return new ConcreteBuilder();
+	   }
+
+	   public Builder<?> toBuilder() {
+	      return new ConcreteBuilder().fromAttributes(this);
+	   }
+	
+	public abstract static class Builder<T extends Builder<T>> extends Resource.Builder<T> 
+	{
+		protected String progress;
+		protected String status;
+		protected String configDrive;
+		protected String tenantId;
+		protected String hostId;
+		protected String created;
+		protected String userId;
+		
+		public T progress(String progress) {
+			this.progress = progress;
+			return self();
+		}
+		
+		public T status(String status) {
+			this.status = status;
+			return self();
+		}
+
+		public T configDrive(String configDrive) {
+			this.configDrive = configDrive;
+			return self();
+		}
+
+		public T tenantId(String tenantId) {
+			this.tenantId = tenantId;
+			return self();
+		}
+
+		public T hostId(String hostId) {
+			this.hostId = hostId;
+			return self();
+		}
+
+		public T created(String created) {
+			this.created = created;
+			return self();
+		}
+
+		public T userId(String userId) {
+			this.userId = userId;
+			return self();
+		}
+
+		
+		public Attributes build() {
+			return new Attributes(id,name,links, progress, status, configDrive, tenantId,
+					hostId, created,userId);
+		}
+		
+		 public T fromAttributes(Attributes in) {
+	         return this.fromResource(in)
+	        	   .progress(in.getProgress())
+	               .status(in.getStatus())
+	               .configDrive(in.getConfigDrive())
+	               .tenantId(in.getTenantId())
+	               .hostId(in.getHostId())
+	               .created(in.getCreated())
+	               .userId(in.getUserId());
+	      }
+	}
+	
+	private static class ConcreteBuilder extends Builder<ConcreteBuilder> {
+	      @Override
+		protected ConcreteBuilder self() {
+			return this;
+		}
 	}
 
 }
